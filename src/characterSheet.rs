ff189118bbb81787d8ee10fab169fe6b89958448
Trenton Scott <InsaneGriot@Outlook.com>
2023-09-09 21:23:58 -0400

    made impl to charactersheet no work done

--- src/characterSheet.rs
+++ src/characterSheet.rs
@@ -2,9 +2,29 @@ use statslibrary::*;
 mod statslibrary;
 
 pub struct Character {
+
+
     name: String,
     stats: statslibrary::CharacterStats,
     species: Species,
+}
+impl Character {
+    fn skill_check(&self) {
+
+    
+    }
+    
+    fn saving_throws(&self) {
+    
+    
+    }
+    
+    fn combat_rounds(&self) {
+    
+        
+    
+    }
+
 }
 
 pub enum Species {
